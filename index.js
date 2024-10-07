fccede21d14d250f3ee853eab50998aeb07de88d
redaaa <155338960+sunsparks33@users.noreply.github.com>
2024-10-07 03:50:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to BRIMO: http://localhost:${port}`);
-  console.log(`🔗 Powered By BRIMO`);
+  console.log(`🔗 Listening to redaaa: http://localhost:${port}`);
+  console.log(`🔗 Powered By redaaa & ViVi`);
 });
 
 
-const statusMessages = ["Ramadhan Karim","Best Discord Bot"];
+const statusMessages = ["Made By redaaa & ViVi"];
 
 
 let currentIndex = 0;
